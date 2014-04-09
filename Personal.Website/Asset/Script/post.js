f46da6e193f8372807a654ea06c1eb5862d381f2
James Dibble <james@jdibble.co.uk>
2014-04-09 18:21:06 +0100

    Blog sidebar date selector fixed

--- Personal.Website/Asset/Script/post.js
+++ Personal.Website/Asset/Script/post.js
@@ -1,4 +1,24 @@
-﻿(function () {
+﻿$(function() {
+    if (window.location.pathname.match('^/blog/[0-9]{4}$') || window.location.pathname.match('^/blog/[0-9]{4}/')) {
+        var year = window.location.pathname.split("/", 3)[2];
+
+        $(['.blog-header-year li a[data-year="', year, '"]'].join('')).parent().addClass("active");
+    }
+
+    if (window.location.pathname.match('^/blog/[0-9]{1,2}$')) {
+        var month = window.location.pathname.split("/", 3)[2];
+
+        $(['.blog-header-month li a[data-month="', month, '"]'].join('')).parent().addClass("active");
+    }
+
+    if (window.location.pathname.match('^/blog/[0-9]{4}/[0-9]{1,2}$') || window.location.pathname.match('^/blog/[0-9]{4}/[0-9]{1,2}/')) {
+        var month = window.location.pathname.split("/", 4)[3];
+
+        $(['.blog-header-month li a[data-month="', month, '"]'].join('')).parent().addClass("active");
+    }
+});
+
+(function () {
     var disqus_shortname = document.domain == 'www.jdibble.co.uk' ? 'jdibble' : 'test-jdibble';
 
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
