962f892eae9ce5082b25aaea754c48498fe697b1
james-dibble <james@jdibble.co.uk>
2014-04-16 22:02:04 +0100

    Remove useless javascript files.  Add GA.

--- Personal.Website/Asset/Script/global.js
+++ Personal.Website/Asset/Script/global.js
@@ -1,11 +1,11 @@
-﻿//var _gaq = _gaq || [];
-//_gaq.push(['_setAccount', 'UA-26477644-1']);
-//_gaq.push(['_trackPageview']);
-//(function () {
-//    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-//    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-//})();
+﻿var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-26477644-1']);
+_gaq.push(['_trackPageview']);
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 /* Modernizr 2.7.1 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-shiv-cssclasses-teststyles-css_vhunit-load
