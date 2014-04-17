65afd380cddb31fab4aa4d21cd96144eedff5a52
james-dibble <james@jdibble.co.uk>
2014-04-17 16:50:23 +0100

    Check route for metaweblog

--- Personal.Website/App_Start/RouteConfig.cs
+++ Personal.Website/App_Start/RouteConfig.cs
@@ -9,6 +9,8 @@
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add(new Route("metaweblog", new MetaWeblogHandler()));
                         
             routes.MapRoute(
                 "Contact",
