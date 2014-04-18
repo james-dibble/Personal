aa702a860878470fc2bb8add3899d10e452c208e
james-dibble <james@jdibble.co.uk>
2014-04-18 14:38:12 +0100

    Once again overriding the metaweblog route

--- Personal.Website/App_Start/RouteConfig.cs
+++ Personal.Website/App_Start/RouteConfig.cs
@@ -9,6 +9,11 @@
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Home",
+                "",
+                new { controller = "Home", action = "Index" });
                                                 
             routes.MapRoute(
                 "Contact",
