e83e51cf2fbb01b929cf8e7f2985d3761368518a
james-dibble <james@jdibble.co.uk>
2014-04-25 22:10:00 +0100

    Add tag highlighting

---
+++ Personal.Website/Asset/Script/post.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    if (window.location.pathname.match('^/blog/[0-9]{4}$') || window.location.pathname.match('^/blog/[0-9]{4}/')) {
+        var year = window.location.pathname.split("/", 3)[2];
+
+        $(['.blog-header-year li a[data-year="', year, '"]'].join('')).parent().addClass("active");
+    }
+
+    if (window.location.pathname.match('^/blog/[0-9]{1,2}$')) {
+        var month = window.location.pathname.split("/", 3)[2];
+
+        $(['.blog-header-month li a[data-month="', month, '"]'].join('')).parent().addClass("active");
+    }
+
+    if (window.location.pathname.match('^/blog/[0-9]{4}/[0-9]{1,2}$') || window.location.pathname.match('^/blog/[0-9]{4}/[0-9]{1,2}/')) {
+        var month = window.location.pathname.split("/", 4)[3];
+
+        $(['.blog-header-month li a[data-month="', month, '"]'].join('')).parent().addClass("active");
+    }
+
+    if (window.location.pathname.match('^/(portfolio|blog)/tag/')) {
+        var tag = window.location.pathname.split("/", 3)[2];
+
+        $(['#tags li a[data-tag="', tag, '"]'].join('')).parent().addClass("active");
+    }
+
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+    $('div[data-src]').imageLoader();
+});
+
+(function () {
+    var disqus_shortname = document.domain == 'www.jdibble.co.uk' ? 'jdibble' : 'test-jdibble';
+
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
